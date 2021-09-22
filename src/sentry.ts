da25c0fba6df110d6624d1aa50259844417bc96f
Ale Ornelas <me@ale.world>
2021-09-22 16:54:42 -0500

    fix: sentry init

--- src/sentry.ts
+++ src/sentry.ts
@@ -5,10 +5,12 @@ import { config } from './config'
 // @ts-expect-error
 global.__rootdir__ = __dirname ?? process.cwd()
 
-export const sentry = Sentry.init({
+Sentry.init({
   dsn: config.sentryDsn,
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0
 })
+
+export const sentry = Sentry
